bccc15ca56cc0e5def6ad6be6f16664856588c42
phanir <phanir@microsoft.com>
2021-02-15 21:39:48 +0530

    Create AzureCloudShell.ps1

---
+++ Chapter01/AzureCloudShell.ps1
@@ -0,0 +1,13 @@
+﻿$appId=""
+$appSecret=""
+$tenantId=""
+$subscriptionName=""
+$resourceGroup = "CookbookRG"
+
+az login --service-principal --username $appId --password $appSecret --tenant $TenantId
+
+az account set --subscription $subscriptionName
+
+az group create --name $resourceGroup --location "East US"
+
+az databricks workspace create --resource-group $resourceGroup --name BigDataWorkspace --location "East US" --sku standard
