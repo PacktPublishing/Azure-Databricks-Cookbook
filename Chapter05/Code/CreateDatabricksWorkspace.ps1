5babf58922b44a212e31f140302922011c60c8e8
abhisheknagpurkar <aanagpurkar@gmail.com>
2023-06-24 22:08:18 +0530

    Update CreateDatabricksWorkspace.ps1
    updated parameters

--- Chapter05/Code/CreateDatabricksWorkspace.ps1
+++ Chapter05/Code/CreateDatabricksWorkspace.ps1
@@ -1,15 +1,15 @@
 # Change the APPId, AppSecret, TenantId, SubscriptionName and ResourceGroup Name
-$appId=""
-$appSecret=""
-$tenantId=""
-$subscriptionName="Pay As You Go"
-$resourceGroup = "CookbookRG"
+$appId="4435e54f-4131-4b0f-be15-141f5af8d984"
+$appSecret="FYo8Q~980a5PL9On45NwDYF3Uu~aTXKlHwNNiaje"
+$tenantId="91006505-05ea-40ff-97ac-20e0378eb509"
+$subscriptionName="Free Trial"
+$resourceGroup = "cookbookan"
 
 az login --service-principal --username $appId --password $appSecret --tenant $TenantId
 
 az account set --subscription $subscriptionName
 
-cd "C:\Users\admin\Azure Databricks Book\Chapter05\Code" #Location where the code is saved in local drive
+cd "/Users/abhisheknagpurkar/Downloads/Azure-Databricks-Cookbook-main/Chapter05/Code" #Location where the code is saved in local drive
 
 az deployment group create --resource-group $resourceGroup --template-file azuredeploy.json --parameters azuredeploy.parameters.json
 
